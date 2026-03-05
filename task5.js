d4ac3b26bf783f36c4326e124e81be038c3dddf9
DibajitRoy <dibajit.2003@gmail.com>
2026-03-05 22:17:18 +0600

    task5

---
+++ task5.js
@@ -0,0 +1,14 @@
+let stu =["dibbo" ,"maruf ","maruf2","osman"];
+for (let i=0; i<stu.length; i++){
+    console.log(stu[i]);
+}
+for(let i=0; i<stu.length; i++){
+    console.log(stu[i]+ " is a student");
+}
+if(stu.length>2){
+    console.log("greter then 2");
+
+}
+else{
+    console.log("no greter than 2");
+}
